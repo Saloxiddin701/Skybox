68217af3e8896b50e57ff228cdf21dd3874a2b62
Saloxiddin701 <abduraxmon.69list.ru@gmail.com>
2022-01-11 21:31:06 +0500

    skybox

---
+++ js/main.js
@@ -0,0 +1,47 @@
+//counter
+function animateValue(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById("value");
+    var timer = setInterval(function () {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+
+function animateId(id, start, end, duration) {
+    if (start === end) return;
+    var range = end - start;
+    var current = start;
+    var increment = end > start? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById("id");
+    var timer = setInterval(function () {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 100000, 169872 ,2000);
+animateId("id", 100000, 168564 ,2000);
+//counter end
+
+//navbar
+const navItem = document.querySelector(".nav-item");
+const burger = document.querySelector(".burger");
+
+burger.addEventListener('click', mobileMenu);
+function mobileMenu() {
+    burger.classList.toggle("active")
+    navItem.classList.toggle("active")
+}
